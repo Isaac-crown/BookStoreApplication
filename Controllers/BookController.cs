59e446b02075417f76fd4210261e7e0436a70a76
godstar <sengodstar@gmail.com>
2022-06-28 04:50:46 +0100

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using BookStoreApplication.Models;
+using BookStoreApplication.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BookStoreApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRespository bookRespository = null;
+       
+        public BookController()
+        {
+            bookRespository =new BookRespository();
+        }
+
+        public ViewResult   GetAllBooks()
+        {
+            var data = bookRespository.GetAllBooks();
+
+            return View();
+        }
+ 
+        public BookModel GetBook(int id)
+        {
+            return bookRespository.GetBooksById(id);
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return bookRespository.SearchBook(bookName, authorName); 
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+           
+            return View();
+        }
+
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
